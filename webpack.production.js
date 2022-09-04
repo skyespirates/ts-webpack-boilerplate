a855ecc539a95140ec90e6867156d687bd29822c
Skyes Crawford <akashiseijurou414@gmail.com>
2022-09-04 13:04:34 +0700

    basic setup for typescript and webpack

---
+++ webpack.production.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const Cleaner = require("clean-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: path.resolve(__dirname, ""),
+  },
+  plugins: [new Cleaner.CleanWebpackPlugin()],
+};
